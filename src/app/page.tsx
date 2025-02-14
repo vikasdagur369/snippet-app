5df911579d4a20775d4d1115d609dcc99b3de6bc
vikasdagur369 <vikasdagur369@gmail.com>
2025-02-14 22:24:06 +0530

    feat: fetch and display snippets on the home page; update component to use async function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { prisma } from "@/lib/prisma";
 
-export default function Home() {
+export default async function Home() {
+
+  const snippets = await prisma.snippet.findMany();
   return (
     <div>
 
@@ -11,6 +14,19 @@ export default function Home() {
         <Link href={"/snippet/new"}><Button>New</Button></Link>
 
       </div>
+      {
+        snippets.map((snippet) => (
+          <div key={snippet.id} className="flex items-center justify-between bg-gray-200 p-2 rounded-md my-2">
+            <h1>{snippet.title}</h1>
+            <Link href={`/snippet/${snippet.id}`}>
+              <Button variant={'link'}>View</Button>
+            </Link>
+          </div>
+        ))
+
+      }
+
+
 
     </div>
 
